b6166f3f802c8a53f4a4bf405d3d93b1f7fc23b2
[FelicitasIslas] <[Islasfelicitas53@gmail.com]>
2026-03-21 13:58:17 -0600

    movil

--- backend/server.js
+++ backend/server.js
@@ -11,16 +11,27 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://cozy-daffodil-cc5699.netlify.app'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
 app.use('/api/registros', registroRoutes);
+
+app.get('/', (req, res) => {
+  res.send('API funcionando correctamente');
+});
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
